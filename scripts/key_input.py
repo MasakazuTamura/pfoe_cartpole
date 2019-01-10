9b328d7181c206fcb94db87fec16036e7cb8d576
MasakazuTamura <s16c1084ng@s.chibakoudai.jp>
2019-01-10 19:00:59 +0900

    Add scripts for ctrl+r, ctrl+s, and ctrl+x

--- scripts/key_input.py
+++ scripts/key_input.py
@@ -3,6 +3,11 @@ import sys,tty,termios
 import rospy
 from std_msgs.msg import Int16
 
+file_directory = "/home/mt/work/pfoe_cartpole/key_log/"
+file_name = "key_cmd_list"
+file_extention = ".txt"
+file_path = file_directory + file_name + file_extention
+
 class _Getch:
     def __call__(self):
         fd = sys.stdin.fileno()
@@ -38,6 +43,7 @@ def get1():
 #        k = inkey()
 #    if k!='':break
     k = inkey()
+    # ^[C or ^[D
     if k == '\x1b':
         for i in range(0, 2):
             k += inkey()
@@ -50,8 +56,21 @@ def get1():
         else:
 #            print("right or left only")
             linear_x = -1
+    # ^C
     elif k == "\x03":
 #        print("ctrl+c key down")
+        linear_x = -1
+    # ^R
+    elif k == "\x12":
+#        print("ctrl+r key down")
+        linear_x = -1
+    # ^S
+    elif k == "\x13":
+#        print("ctrl+s key down")
+        linear_x = -1
+    # ^X
+    elif k == "\x18"
+#        print("ctrl+x key down")
         linear_x = -1
     else:
 #        print("unknown command")
@@ -62,12 +81,41 @@ def get1():
 if __name__ == '__main__':
     rospy.init_node("key_cmd")
     pub = rospy.Publisher("key_in", Int16, queue_size=1)
+    key_cmd_list = []
+    key_replay = False
 
+    key_cmd = Int16()
+    rate = rospy.Rate(8)
     while not rospy.is_shutdown():
-        key_cmd = Int16()
-        keyevent, key_cmd.data = get1()
-#        print(keyevent, key_cmd.data)
-        if keyevent == "\x03":
-            rospy.signal_shutdown("ctrl+c")
-        if not key_cmd.data < 0:
+        if key_replay:
+            key_cmd.data = int(key_cmd_list.pop(0))
+            print(key_cmd.data)
             pub.publish(key_cmd)
+            if not len(key_cmd_list):
+                key_replay = False
+        else:
+            keyevent, key_cmd.data = get1()
+#            print(keyevent, key_cmd.data)
+            # ^C
+            if keyevent == "\x03":
+                rospy.signal_shutdown("ctrl+c")
+            # ^R
+            elif keyevent == "\x12":
+                with open(file_path, mode="r") as f:
+                    key_cmd_list = [s.strip() for s in f.readlines()]
+                key_replay = True
+            # ^S
+            elif keyevent == "\x13":
+                with open(file_path, mode="w") as f:
+                    f.write("\n".join(map(str, key_cmd_list)))
+#                print("saved key_cmd history as {}".formad(file_path))
+                key_cmd_list = []
+            # ^X
+            elif keyevent == "\x18":
+                key_cmd_list = []
+            elif not key_cmd.data < 0:
+                key_cmd_list.append(key_cmd.data)
+                pub.publish(key_cmd)
+
+        rate.sleep()
+
